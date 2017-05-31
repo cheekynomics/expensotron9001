16fcb40e9f7fe153d02d6b818ef5563c96acf6d3
Drum Ogilvie <me@daogilvie.com>
2017-05-31 21:55:32 +0100

    Some brief comments added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -15,6 +15,7 @@ import conToReg from "../../datafiles/conToReg.json";
 import idToName from "../../datafiles/idToName.json";
 
 // Variables
+// Colour palette, used for region colouration.
 const cheekyPalette = [
   "rgb(148,166,253)",
   "rgb(3,98,160)",
@@ -26,11 +27,14 @@ const cheekyPalette = [
   "rgb(141,25,147)",
   "rgb(247,94,240)"
 ];
+
+// Define the viewbox width and height so we don't need to worry
+// about real-pixel sizes. These are actually used later.
 let vbWidth = 600;
 let vbHeight = 1000;
 
+// Load in the topological data.
 let eeRegions = topojson.feature(regionsTopo, regionsTopo.objects.eer).features;
-
 let wales = topojson.feature(walesTopo, walesTopo.objects.eer).features[0];
 let scotland = topojson.feature(scotlandTopo, scotlandTopo.objects.eer)
   .features[0];
@@ -45,39 +49,62 @@ Presentation Component
 */
 class MapView extends Component {
   render() {
-    this._bounds = {};
+    // Map region IDs to the cheeky palette colour.
     let colours = d3.scaleOrdinal(cheekyPalette).domain(_.keys(idToName));
+
+    // Set the map projection to be a good one for the UK.
     let projection = d3
       .geoAlbers()
       .parallels([50, 60]) // Conic parallels (latitudes) that sandwich GBR for minimal distortion.
       .rotate([-4, 0]); // Rotate to minimise distortion over the UK.
+
+    // Use D3's path generator.
     let geoPath = d3.geoPath().projection(projection);
-    // Set the bounds for the projection
+
+    // Set the extent for the projection, giving us a 20 pixel pad
+    // from our viewbox width.
     let pad = 20;
     projection.fitExtent(
       [[pad, pad], [vbWidth - pad, vbHeight - pad]],
       constituencies_obj
     );
+
+    // This object will store the bounds for each electoral region
+    // for zooming purposes.
+    this._bounds = {};
+    // TODO: find a way to do this that doesn't keep the region
+    // data hanging around. Currently
     eeRegions.forEach(region => {
       this._bounds[region.id] = geoPath.bounds(region);
     });
 
+    // This map generates an SVG path JSX element for each constituency
+    // we have shape data for.
     let conPaths = constituencies.map(con => {
+      // Get the region id.
       let reg = conToReg[con.id].reg;
+      // Is this region focused?
       let isFocused = this.props.focusedRegion === reg;
+      // Here we set our css classes based on 2 nested ternary
+      // statements. The outer one just determines whether we
+      // need any CSS at all (if we're not zoomed then we don't).
+      // If we are zoomed we definitely add 'zoomed'.
+      // If we *are* zoomed, then the second inner statement checks
+      // if this is the focused region, and adds the 'focused' class.
       let zoomClass = this.props.focusedRegion !== null
         ? isFocused ? "zoomed focused" : "zoomed"
         : "";
 
+      // We return the 'path' element here.
       return (
         <path
-          vectorEffect="non-scaling-stroke"
-          key={con.id}
-          className={`constituency ${con.id} ${zoomClass}`}
-          d={geoPath(con)}
-          fill={colours(reg)}
+          vectorEffect="non-scaling-stroke" // Stroke edges don't scale! Super handy.
+          key={con.id} // When producing React components in lists, each one needs a unique key.
+          className={`constituency ${con.id} ${zoomClass}`} // CSS class names
+          d={geoPath(con)} // We use D3's path generator directly here.
+          fill={colours(reg)} // We set the fill colour here too.
           onClick={event => {
-            event.stopPropagation();
+            event.stopPropagation(); // Prevent the SVG click handler from also picking this up.
             this.props.selectRegion(reg);
           }}
         />
@@ -101,6 +128,9 @@ class MapView extends Component {
     );
   }
 
+  // This functoin contains the same zoom transform logic as before
+  // but now we return a CSS transform string directly as we are
+  // doing all transitions in CSS.
   _calculateTransform() {
     if (this.props.focusedRegion !== null) {
       let bounds = this._bounds[this.props.focusedRegion];
@@ -116,6 +146,8 @@ class MapView extends Component {
   }
 }
 
+// This is an optional but ESLINT enforced mapping of
+// props to object types. It is good practice.
 MapView.propTypes = {
   focusedRegion: PropTypes.string,
   selectRegion: PropTypes.func,
@@ -127,9 +159,11 @@ MapView.propTypes = {
 Containers and associated bits.
 */
 
+// String constants to reduce opportunity for mistakes.
 const REGION_SELECTED_TYPE = "REGION_SELECTED";
 const REGION_UNSELECTED_TYPE = "REGION_UNSELECTED";
 
+// This function, and the one below, produce redux actions.
 const REGION_SELECTED = region_id => {
   return {
     type: REGION_SELECTED_TYPE,
@@ -143,6 +177,8 @@ const REGION_UNSELECTED = () => {
   };
 };
 
+// This is a reducer, imported in reducers.js, that is used
+// to manage the redux state for selected regions.
 const selected_region = (state = null, action) => {
   switch (action.type) {
   case REGION_SELECTED_TYPE:
@@ -158,6 +194,9 @@ const selected_region = (state = null, action) => {
   }
 };
 
+// These two functions supply the needed mapping for
+// data flowing in (statetoprops), and actions flowing out
+// (dispatchtoprops)
 const mapStateToProps = state => {
   return {
     focusedRegion: state.selected_region
